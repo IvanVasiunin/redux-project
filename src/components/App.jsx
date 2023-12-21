e440119387017d1242dfc26ec78b5ac84b15e58f
Ivan <ivanvasyunin@gmail.com>
2023-12-21 17:32:09 +0100

    initial files have been added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
-export const App = () => {
+import Counter from './Counter';
+
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Counter />
   );
-};
+}
+
+export default App;
