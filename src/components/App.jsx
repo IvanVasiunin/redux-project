37e85b63c0e0c4f11396cb9df2a793542841deb5
Ivan <ivanvasyunin@gmail.com>
2023-12-23 17:23:37 +0100

    auth and header components added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
+import { Fragment } from 'react';
 import Counter from './Counter';
-
+import Header from './Header';
+import Auth from './Auth';
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
   );
 }
 
